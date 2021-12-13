b306ed4a0281ec0638612ec6ee7b0a6dce5c14a1
Vítor Beneti Martins <61762667+benetche@users.noreply.github.com>
2021-12-12 22:05:21 -0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-# criando pastas para as libs
-
-mkdir lib
-mkdir include
-
-unzip raylib-master.zip
-cd ./raylib-master/src && make PLATFORM=PLATFORM_DESKTOP
-mv raylib.h ../../include
-cd ../ && mv libraylib.a ../lib
